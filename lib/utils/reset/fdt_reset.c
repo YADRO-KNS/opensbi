37c094a44dd5557f8bef990562d92ef17bbbfc75
Nikita Shubin <n.shubin@yadro.com>
2021-09-23 19:21:31 +0300

    lib: utils/reset: Add generic da9063 reset driver
    da9063 PMIC can be used to reset/shutdown the
    Sifive Unmatched board.
    
    shutdown is done simply by writing SHUDOWN bit to
    DA9063_REG_CONTROL_F register.
    
    reset via setting WAKEUP bit in DA9063_REG_CONTROL_F
    register followed by masking POWER and POWER1 domains
    and setting STANDBY bit in DA9063_REG_CONTROL_A,
    originally discovered by Alexandre Ghiti on linux-riscv
    maillists.
    
    Signed-off-by: Nikita Shubin <n.shubin@yadro.com>

--- lib/utils/reset/fdt_reset.c
+++ lib/utils/reset/fdt_reset.c
@@ -18,6 +18,7 @@ extern struct fdt_reset fdt_reset_htif;
 extern struct fdt_reset fdt_reset_sifive_test;
 extern struct fdt_reset fdt_reset_sunxi_wdt;
 extern struct fdt_reset fdt_reset_thead;
+extern struct fdt_reset fdt_reset_da9063;
 
 static struct fdt_reset *reset_drivers[] = {
 	&fdt_poweroff_gpio,
@@ -26,6 +27,7 @@ static struct fdt_reset *reset_drivers[] = {
 	&fdt_reset_sifive_test,
 	&fdt_reset_sunxi_wdt,
 	&fdt_reset_thead,
+	&fdt_reset_da9063,
 };
 
 int fdt_reset_init(void)
